f65596c4f9fc0c0bb09dee4fcf7a2b5686d587c3
Белехова   Екатерина <e.belekhova@192.168.0.12>
2026-02-19 22:31:38 +0300

    feat: add basic project structure

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+    console.log('Login function called');
+    alert('Login functionality - to be implemented');
+}
+
+// TODO: Add validation
+// FIXME: Safari compatibility issue
